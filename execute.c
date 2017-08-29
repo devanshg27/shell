43d3034c3260fd507d361d49ab90e2110e9e8bf4
Devansh Gautam <devanshg27@gmail.com>
2017-08-30 03:03:02 +0530

    Added Background processes with exit message

--- execute.c
+++ execute.c
@@ -1,9 +1,22 @@
 #include <execute.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+void runCommandinBackground(char **arguments) {
+	pid_t pid = fork();
+	if(pid==0){
+		execvp(arguments[0], arguments);
+	}
+	else{
+		wait(NULL);
+		fprintf(stderr, "%s with pid %d exited normally\n", arguments[0], pid);
+		exit(0);
+	}
+}
 
 void runCommand(char *command){
 	int BLOCK_SIZE = 100, BUFFER_SIZE = 100, position = 0;
@@ -21,11 +34,21 @@ void runCommand(char *command){
 		argument = strtok(NULL, " \t");
 	}
 	arguments[position] = NULL;
-	pid_t pid = fork();
-	if(pid==0){
-		execvp(arguments[0], arguments);
+	if(strcmp(arguments[position-1], "&") == 0) {
+		--position;
+		arguments[position] = NULL;
+		pid_t pid = fork();
+		if(pid==0){
+			runCommandinBackground(arguments);
+		}
 	}
 	else{
-		wait(NULL);
+		pid_t pid = fork();
+		if(pid==0){
+			execvp(arguments[0], arguments);
+		}
+		else{
+			wait(NULL);
+		}
 	}
 }
